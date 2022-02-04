13ea9f6a9aa9b49080d4377fa6fc15b78a4ec62b
alpinexswift <alpinexswift@gmail.com>
2022-02-04 20:34:28 +0600

    i added another line of code

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public static Hello {
     public static void main(string[] args) {
         System.out.println("This is a java code ........");
+
+        greeting("Alpine Swift");
+    }
+
+    public static void greeting(String name){
+        System.out.println("welcome " + name);
     }
 }
